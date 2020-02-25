12a243febf75f9dd0eb2a6f1f19cb3c45701b08e
Jerome Aglugub <jaaglugub@clc.pshs.edu.ph>
2020-02-25 09:48:50 +0800

    Lots of Changes for Emailing

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,23 +8,42 @@ class ComposerStaticInit1fd9f519df1a15af935158ffc4ecc8f4
 {
     public static $files = array (
         '3f8bdd3b35094c73a26f0106e3c0f8b2' => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/SendGrid.php',
-        '9dda55337a76a24e949fbcc5d905a2c7' => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/helpers/mail/Mail.php',
     );
 
-    public static $prefixesPsr0 = array (
+    public static $prefixLengthsPsr4 = array (
         'S' => 
         array (
-            'SendGrid' => 
-            array (
-                0 => __DIR__ . '/..' . '/sendgrid/php-http-client/lib',
-            ),
+            'SendGrid\\Stats\\' => 15,
+            'SendGrid\\Mail\\' => 14,
+            'SendGrid\\Contacts\\' => 18,
+            'SendGrid\\' => 9,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'SendGrid\\Stats\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/stats',
+        ),
+        'SendGrid\\Mail\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/mail',
+        ),
+        'SendGrid\\Contacts\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/contacts',
+        ),
+        'SendGrid\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/sendgrid/php-http-client/lib',
         ),
     );
 
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixesPsr0 = ComposerStaticInit1fd9f519df1a15af935158ffc4ecc8f4::$prefixesPsr0;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit1fd9f519df1a15af935158ffc4ecc8f4::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit1fd9f519df1a15af935158ffc4ecc8f4::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
