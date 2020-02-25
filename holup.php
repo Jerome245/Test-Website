da5adad8582d6b934ce49b69035bd7445f40c97d
Jerome Aglugub <jaaglugub@clc.pshs.edu.ph>
2020-02-25 08:38:10 +0800

    Update holup.php

--- holup.php
+++ holup.php
@@ -4,18 +4,18 @@
 		ini_set("display_errors", 1);
 		require 'vendor/autoload.php';
 
-					$from = new SendGrid\Email(null, "test@example.com");
-					$subject = "Hello World from the SendGrid PHP Library!";
-					$to = new SendGrid\Email(null, "jaaglugub@clc.pshs.edu.ph");
-					$content = new SendGrid\Content("text/plain", "Hello, Email!");
-					$mail = new SendGrid\Mail($from, $subject, $to, $content);
+		$from = new SendGrid\Email(null, "test@example.com");
+$subject = "Hello World from the SendGrid PHP Library!";
+$to = new SendGrid\Email(null, "jaaglugub@clc.pshs.edu.ph");
+$content = new SendGrid\Content("text/plain", "Hello, Email!");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-					$apiKey = getenv('SENDGRID_API_KEY');
-					$sg = new \SendGrid($apiKey);
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
 
-					$response = $sg->client->mail()->send()->post($mail);
-					$response->statusCode();
-					$response->headers();
-					$response->body();
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+echo $response->headers();
+echo $response->body();
 
 ?>
