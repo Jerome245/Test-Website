21f4c4388144ff1d363e2e5f8da0bfb7469e7c4d
Jerome Aglugub <jaaglugub@clc.pshs.edu.ph>
2020-02-25 09:58:31 +0800

    1

--- holup.php
+++ holup.php
@@ -1,26 +1,27 @@
 <?php
+require 'vendor/autoload.php'; // If you're using Composer (recommended)
+// Comment out the above line if not using Composer
+// require("<PATH TO>/sendgrid-php.php");
+// If not using Composer, uncomment the above line and
+// download sendgrid-php.zip from the latest release here,
+// replacing <PATH TO> with the path to the sendgrid-php.php file,
+// which is included in the download:
+// https://github.com/sendgrid/sendgrid-php/releases
 
-		error_reporting(E_ALL);
-		ini_set("display_errors", 1);
-		require 'vendor/autoload.php';
-
-		$from = new SendGrid\Email(null, "test@example.com");
-$subject = "Hello World from the SendGrid PHP Library!";
-$to = new SendGrid\Email(null, "jaaglugub@clc.pshs.edu.ph");
-$content = new SendGrid\Content("text/plain", "Hello, Email!");
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
-
-$apiKey = getenv('SENDGRID_API_KEY');
-$sg = new \SendGrid($apiKey);
-
-echo $apiKey;
-echo "\n\n\n";
-
-$response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-echo "\n\n\n                             ";
-echo $response->headers();
-echo "\n\n\n                             ";
-echo $response->body();
-
-?>
+$email = new \SendGrid\Mail\Mail();
+$email->setFrom("test@example.com", "Example User");
+$email->setSubject("Sending with SendGrid is Fun");
+$email->addTo("test@example.com", "Example User");
+$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+$email->addContent(
+    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+);
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
+}
