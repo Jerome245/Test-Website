4de5239d82306ca7f950b0026ce64c702f79f529
Jerome Aglugub <jaaglugub@clc.pshs.edu.ph>
2020-02-25 10:48:38 +0800

    Test Again

--- holup.php
+++ holup.php
@@ -9,21 +9,19 @@ require 'vendor/autoload.php'; // If you're using Composer (recommended)
 // https://github.com/sendgrid/sendgrid-php/releases
 
 $email = new \SendGrid\Mail\Mail();
-$email->setFrom("emailbotsender@gmail.com", "Example User");
+$email->setFrom("test@example.com", "Example User");
 $email->setSubject("Sending with SendGrid is Fun");
-$email->addTo("jaaglugub@clc.pshs.edu.ph", "Example User");
+$email->addTo("test@example.com", "Example User");
 $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
 $email->addContent(
     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
 );
-$sendgrid = new \SendGrid(getenv('dataiswhatineed'));
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
 try {
     $response = $sendgrid->send($email);
     print $response->statusCode() . "\n";
     print_r($response->headers());
     print $response->body() . "\n";
-    echo "HI!";
 } catch (Exception $e) {
     echo 'Caught exception: '. $e->getMessage() ."\n";
-    echo "NO!";
 }
