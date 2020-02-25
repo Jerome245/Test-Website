b2b99a0f28d2685ca6ae06882725f3c24d16244f
Jerome Aglugub <jaaglugub@clc.pshs.edu.ph>
2020-02-25 08:34:19 +0800

    Typo & Misclick
    Mixed File names

---
+++ holup.php
@@ -0,0 +1,21 @@
+<?php
+
+		error_reporting(E_ALL);
+		ini_set("display_errors", 1);
+		require 'vendor/autoload.php';
+
+					$from = new SendGrid\Email(null, "test@example.com");
+					$subject = "Hello World from the SendGrid PHP Library!";
+					$to = new SendGrid\Email(null, $email);
+					$content = new SendGrid\Content("text/plain", "Hello, Email!");
+					$mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+					$apiKey = getenv('SENDGRID_API_KEY');
+					$sg = new \SendGrid($apiKey);
+
+					$response = $sg->client->mail()->send()->post($mail);
+					$response->statusCode();
+					$response->headers();
+					$response->body();
+
+?>
