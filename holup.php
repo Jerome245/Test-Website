2b3cb5214170381cfc47481e86e0cd2ef6d523e9
Jerome Aglugub <jaaglugub@clc.pshs.edu.ph>
2020-02-25 08:35:49 +0800

    Update holup.php

--- holup.php
+++ holup.php
@@ -6,7 +6,7 @@
 
 					$from = new SendGrid\Email(null, "test@example.com");
 					$subject = "Hello World from the SendGrid PHP Library!";
-					$to = new SendGrid\Email(null, $email);
+					$to = new SendGrid\Email(null, "jaaglugub@clc.pshs.edu.ph");
 					$content = new SendGrid\Content("text/plain", "Hello, Email!");
 					$mail = new SendGrid\Mail($from, $subject, $to, $content);
 
